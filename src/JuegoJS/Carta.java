8e1836e5fa8d6ffa3be2a3fad83cec6d8950b0f9
h0ffm4n <hoffman.yeah@gmail.com>
2014-07-26 22:41:15 +0200

    estadisticas de las cartas

--- src/JuegoJS/Carta.java
+++ src/JuegoJS/Carta.java
@@ -14,8 +14,9 @@ class Carta {
    
     int id;
     String Descripcion;
-    int Dmg;
+    int DmgAire,DmgLight,DmgBlindado;
     int Def;
+    int vida;
      
    Carta(int i){
       switch(i)
@@ -23,11 +24,29 @@ class Carta {
           case 0:
               this.id=0;
               this.Descripcion="soldado usa";
-              break;
+              this.DmgAire=1;
+              this.DmgLight=5;
+              this.DmgBlindado=2;
+              this.vida=10;
               
           case 1: 
               this.id=1;
-              this.Descripcion=""
+              this.Descripcion="mortero M224 60mm";
+              this.DmgAire=0;
+              this.DmgLight=8;
+              this.DmgBlindado=3;
+              this.vida=6;
+              break;
+              
+          case 2:
+              this.id=2;
+              this.tipo
+              this.Descripcion="M1 Abrams";
+              this.DmgAire=0;
+              this.DmgLight=8;
+              this.DmgBlindado=3;
+              this.vida=12
+              break;
       }
        
    }
